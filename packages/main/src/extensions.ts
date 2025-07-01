69d8bb6d9b785fc19c3cc084eab16f4050962e7c
nihil-pro <Romanolga13>
2025-07-01 20:29:19 +0300

    fixed bug when values weren't made observable if they weren't observed

--- packages/main/src/extensions.ts
+++ packages/main/src/extensions.ts
@@ -3,41 +3,27 @@ export function set(i: number, value: unknown) {
 }
 
 export function equal(input: any) {
-  try {
-    if (input == null) {
-      return false;
+  if (input == null) return false;
+  const voi = input.valueOf();
+  const vot = this.valueOf();
+  if (typeof vot === 'object') {
+    if (typeof voi !== 'object') return false;
+    const keys = Object.keys(vot);
+    if (keys.length !== Object.keys(voi).length) return false;
+    for (const key of keys) {
+      const $vot = vot[key];
+      const $voi = voi[key];
+      if ($vot == null) {
+        // eslint-disable-next-line max-depth,no-lonely-if
+        if ($voi != null) return false;
+      } else {
+        // eslint-disable-next-line max-depth,no-lonely-if
+        if (!$vot.equal($voi)) return false;
+      }
     }
-    const voi = input.valueOf();
-    const vot = this.valueOf();
-    if (typeof vot === 'object') {
-      if (typeof voi !== 'object') {
-        return false;
-      }
-      const keys = Object.keys(vot);
-      if (keys.length !== Object.keys(voi).length) {
-        return false;
-      }
-      for (const key of keys) {
-        const $vot = vot[key];
-        const $voi = voi[key]; // Reflect.getOwnPropertyDescriptor(voi, key)?.value;
-        if ($vot == null) {
-          // eslint-disable-next-line max-depth,no-lonely-if
-          if ($voi != null) {
-            return false;
-          }
-        } else {
-          // eslint-disable-next-line max-depth,no-lonely-if
-          if (!$vot.equal($voi)) {
-            return false;
-          }
-        }
-      }
-      return true;
-    }
-    return Object.is(vot, voi);
-  } catch {
-    return false;
+    return true;
   }
+  return Object.is(vot, voi);
 }
 
 declare global {
