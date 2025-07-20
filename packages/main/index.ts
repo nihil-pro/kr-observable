6562701e8dfade059723662e296139f71f52174d
nihil-pro <Romanolga13>
2025-07-20 13:36:06 +0300

    #44 fixed Array.set type declaration

--- packages/main/index.ts
+++ packages/main/index.ts
@@ -1,3 +1,11 @@
 export { Observable, makeObservable } from './src/Observable.js';
 export { subscribe, listen, transaction, autorun } from './src/api.js';
 export { executor } from './src/global.this.js';
+
+
+declare global {
+  interface Array<T> {
+    set(i: number, v: T): void;
+  }
+}
+
