d0d18430519ea3e6ca00dd1032c3ff74851905a8
nihil-pro <Romanolga13>
2026-01-19 21:13:31 +0300

    #63 fixed subscription to map entries

--- packages/main/src/Observable.map.ts
+++ packages/main/src/Observable.map.ts
@@ -1,64 +1,65 @@
 import { Admin } from './Admin.js';
 import { Global } from './global.js';
 import { Utils } from './Utils.js';
+import { Property } from './types';
 
-function getKey(metaKey: string, key: unknown): any {
-  if (key == undefined) return `${metaKey}.${key}`;
-  if (typeof key === 'object') return key;
-  if (typeof key === 'symbol') return `${metaKey}.${key.description}`;
-  return `${metaKey}.${key}`;
-}
 
+/** A side effect which depends on Map will run depending on how it reads map entries:
+ * - `map.has(key)` or `map.get(key)` - runs when an entry with this key is added or removed,
+ * or when the value for this key is replaced with a new one.
+ * - `map.keys()` – runs when the map size changes (an entry is added or removed).
+ * - `map.values()` - runs when the map size changes, or when any value in the map is replaced with a new one.
+ * - `map.entries()` - same as `map.values()` (runs on size changes or any value replacement).
+ */
 export class ObservableMap<K, V> extends Map<K, V> {
+  static #getKey(metaKey: string, key: unknown): Property {
+    if (key == undefined) return `${metaKey}.${key}`;
+    if (typeof key === 'object') return key as unknown as Property;
+    if (typeof key === 'symbol') return `${metaKey}.${key.description}`;
+    return `${metaKey}.${key}`;
+  }
+
   get meta() {
     return Global.meta.get(this) || Admin.meta;
   }
 
   get size() {
-    this.reportRead(`${this.meta.key}.keys`);
+    Global.executor.report(this.meta.adm, `${this.meta.key}.keys`);
     return super.size;
   }
 
-  reportRead(key: any) {
-    Global.executor.report(this.meta.adm, key);
-  }
-
-  report(key: K, value?: V) {
-    this.meta.adm.report(getKey(this.meta.key, key), value);
-  }
-
   keys() {
-    this.reportRead(`${this.meta.key}.keys`);
+    Global.executor.report(this.meta.adm, `${this.meta.key}.keys`);
     return super.keys();
   }
 
   entries() {
-    this.reportRead(`${this.meta.key}.keys`);
+    Global.executor.report(this.meta.adm, `${this.meta.key}.entries`);
     return super.entries();
   }
 
   values() {
-    this.reportRead(`${this.meta.key}.keys`);
+    Global.executor.report(this.meta.adm, `${this.meta.key}.entries`);
     return super.values();
   }
 
   forEach(callback: (value: V, key: K, map: this) => void, thisArg?: any): void {
-    this.reportRead(`${this.meta.key}.keys`);
+    Global.executor.report(this.meta.adm, `${this.meta.key}.entries`);
     return super.forEach(callback, thisArg);
   }
 
   [Symbol.iterator]() {
-    this.reportRead(`${this.meta.key}.keys`);
+    Global.executor.report(this.meta.adm, `${this.meta.key}.entries`);
     return super[Symbol.iterator]();
   }
 
   has(key: K): boolean {
-    this.reportRead(getKey(this.meta.key, key));
+    Global.executor.report(this.meta.adm, ObservableMap.#getKey(this.meta.key, key));
     return super.has(key);
   }
 
   get(key: K): V | undefined {
-    this.reportRead(getKey(this.meta.key, key));
+    Global.executor.report(this.meta.adm, ObservableMap.#getKey(this.meta.key, key));
     return super.get(key);
   }
 
@@ -72,27 +73,38 @@ export class ObservableMap<K, V> extends Map<K, V> {
     const hasKey = super.has(key);
     const prevValue = super.get(key);
     const result = super.set(key, newValue);
-    if (!hasKey) this.meta.adm.report(`${this.meta.key}.keys`, this);
-    if (prevValue !== newValue) this.report(key, value);
+
+    if (!hasKey) {
+      this.meta.adm.report(`${this.meta.key}.entries`, this);
+      this.meta.adm.report(`${this.meta.key}.keys`, this);
+      this.meta.adm.report(ObservableMap.#getKey(this.meta.key, key), newValue);
+    } else if (prevValue !== newValue) {
+      this.meta.adm.report(`${this.meta.key}.entries`, this);
+      this.meta.adm.report(ObservableMap.#getKey(this.meta.key, key), newValue);
+    }
     return result;
   }
 
   delete(key: K) {
     const result = super.delete(key);
     if (result) {
-      this.report(key);
-      this.meta.adm.report(`${this.meta.key}.keys`, this)
+      // This is ok, since relevant side effects will react only on that key,
+      // they are subscribed to
+      this.meta.adm.report(ObservableMap.#getKey(this.meta.key, key), undefined);
+      this.meta.adm.report(`${this.meta.key}.keys`, this);
+      this.meta.adm.report(`${this.meta.key}.entries`, this);
     }
     return result;
   }
 
   clear() {
     if (super.size === 0) return;
-    const key = this.meta.key;
-    for (const _key of this.keys()) {
-      this.meta.adm.report(getKey(key, _key), undefined);
+    const metaKey = this.meta.key;
+    for (const key of this.keys()) {
+      this.meta.adm.report(ObservableMap.#getKey(metaKey, key), undefined);
     }
     super.clear();
-    this.meta.adm.report(`${this.meta.key}.keys`, this)
+    this.meta.adm.report(`${this.meta.key}.keys`, this);
+    this.meta.adm.report(`${this.meta.key}.entries`, this);
   }
 }
