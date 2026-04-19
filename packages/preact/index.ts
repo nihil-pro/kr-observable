c151b372d24754a81f5b9614b80176650c5dc338
nihil-pro <Romanolga13>
2026-04-19 12:29:53 +0300

    Added support for react or preact class components

--- packages/preact/index.ts
+++ packages/preact/index.ts
@@ -1,4 +1,4 @@
-import { executor, Runnable } from 'kr-observable';
+import { executor, ObservableAdmin, Runnable } from 'kr-observable';
 import { VNode, Component } from 'preact';
 
 function shallowDiffers(a: Object, b: Object) {
@@ -8,9 +8,55 @@ function shallowDiffers(a: Object, b: Object) {
 }
 
 export function observer<P>(
-  rc: ((props: P) => VNode<any>),
+  rc: (props: P) => VNode<any>,
+  debug: boolean
+): VNode<any>;
+
+export function observer<T extends new (...args: any[]) => Component<any, any>>(
+  rc: T,
+  debug?: boolean
+): T;
+
+export function observer<P>(
+  rc:
+    | ((props: P) => VNode<any>)
+    | (new (...args: any[]) => Component<any, any>),
   debug = false
-) {
+): any {
+
+  // class component,
+  // can either be Component or PureComponent from "preact/compat" subpackage
+  // to check with instanceof we need to import PureComponent from /compat,
+  // which may include it into bundle, but we want to avoid this.
+  // That why we check it in this way
+  if (typeof rc.prototype.render === 'function') {
+    const ClassComponent = rc as new (...args: any[]) => Component<any, any>;
+
+    // @ts-ignore
+    return class extends ClassComponent implements Runnable {
+      active = false;
+      debug = debug;
+      read?: Set<ObservableAdmin>;
+      deps?: Set<Set<Runnable>>
+      runId = 1;
+      run: Function;
+      subscriber: (changes?: Set<string | symbol>) => void;
+
+      constructor(props: any, state: any) {
+        super(props, state);
+        this.run = this.render;
+        this.render = () => executor.execute(this, this.props)
+        this.subscriber = () => this.forceUpdate();
+
+        // PureComponent has defined shouldComponentUpdate, Component didn't
+        if (typeof rc.prototype.shouldComponentUpdate !== 'function') {
+          this.shouldComponentUpdate = nextProps => shallowDiffers(this.props, nextProps);
+        }
+      }
+    }
+  }
+
+
   // This work in Preact and Preact/compat because it doesn't throw when a hook is called
   // inside a class component
   return class extends Component<P, any> implements Runnable {
@@ -37,4 +83,3 @@ export function observer<P>(
     }
   };
 }
-
